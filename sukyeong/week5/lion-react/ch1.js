8f9fbcb6113234c677a9cdd7b2876891078987d4
Sukyeong Park <zztnrudzz123433@gmail.com>
2021-05-18 17:31:15 +0900

    Create README.md

---
+++ sukyeong/week5/lion-react/ch1.js
@@ -0,0 +1,11 @@
+plusTwo = s => s+2
+
+console.log(plusTwo(10));
+
+arr = [1,2,3,4,5,6,7,8,13,4325,7,232,223]
+
+arr_map = arr.map(value => value*2)
+console.log(arr_map);
+
+arr_filter = arr.filter(value => value>10)
+console.log(arr_filter)
